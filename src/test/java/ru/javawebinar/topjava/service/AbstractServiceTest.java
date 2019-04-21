14656137443f2d95ba3bb84d3720c2610d9c3770
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2019-04-21 22:55:03 +0300

    Fix

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
@@ -15,6 +16,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.TimingRules;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
+
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
@@ -26,13 +30,24 @@ abstract public class AbstractServiceTest {
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
 
+    @Rule
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     static {
         // needed only for java.util.logging (postgres driver)
         SLF4JBridgeHandler.install();
     }
 
-    @Rule
-    public Stopwatch stopwatch = TimingRules.STOPWATCH;
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
+    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
+    public <T extends Throwable> void validateRootCause(Runnable runnable, Class<T> exceptionClass) {
+        try {
+            runnable.run();
+            Assert.fail("Expected " + exceptionClass.getName());
+        } catch (Exception e) {
+            Assert.assertThat(getRootCause(e), instanceOf(exceptionClass));
+        }
+    }
 }
